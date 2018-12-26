76493ead698ec1046a45039577328b3b2badbcf3
Putnam14 <putnambr76@gmail.com>
2018-12-25 17:56:54 -0700

    Styled component for the hero

---
+++ src/components/styles/HeroStyles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const HeroStyles = styled.div`
+  display: grid;
+  height: 100vh;
+  width: 100vw;
+  grid-template-columns: 1fr 1fr;
+  .bio {
+    display: grid;
+    align: center;
+    justify-items: center;
+    align-items: center;
+    height: 100%;
+  }
+`
+
+export default HeroStyles
