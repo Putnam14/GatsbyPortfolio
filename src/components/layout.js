32384145373ecd4002ccef2e9b9fb0ccde35f9d6
Putnam14 <putnambr76@gmail.com>
2018-12-28 01:24:23 -0700

    Manage the theme in a central location

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,32 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import base from './base.css'
-import Container from './container'
+import Helmet from 'react-helmet'
 import Navigation from './navigation'
+import styled, { ThemeProvider } from 'styled-components'
+
+const theme = {
+  base: '#327381',
+  accent: '#A96F51',
+  gray: '#3B4044',
+  lightGray: '#A1A2A7',
+  white: '#F2FDFF',
+  black: '#393939',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+}
+
+const StyledPage = styled.div`
+  background: ${props => props.theme.white};
+  color: ${props => props.theme.gray};
+`
+const Meta = () => (
+  <Helmet>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charSet="utf-8" />
+    <title>Bridger Putnam</title>
+  </Helmet>
+)
 
 class Template extends React.Component {
   render() {
@@ -15,10 +39,13 @@ class Template extends React.Component {
     }
 
     return (
-      <Container>
-        <Navigation />
-        {children}
-      </Container>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Navigation />
+          {children}
+        </StyledPage>
+      </ThemeProvider>
     )
   }
 }
