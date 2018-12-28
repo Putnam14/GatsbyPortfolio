7be1a97c9c382bafa14be6aac6bcd0ddf4370057
Putnam14 <putnambr76@gmail.com>
2018-12-28 01:24:44 -0700

    Styling the hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,26 +1,49 @@
 import React from 'react'
 import Img from 'gatsby-image'
 import HeroStyles from './styles/HeroStyles'
+import Helmet from 'react-helmet'
+import { Link } from 'gatsby'
 
 export default ({ data }) => (
   <HeroStyles>
-    <Img alt={data.name} fluid={data.heroImage.fluid} />
-    <div className="bio">
-      <div>
-        <p>Independent</p>
-        <p>Web Designer</p>
-        <p>Full Stack Developer</p>
-        <p>Digital Strategist</p>
-        <p>
-          I build bad-ass websites that are hella performant. Hire me and let’s
-          build something you can brag about.
-        </p>
-        <button>Talk to me</button>
+    <Helmet>
+      <link
+        rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+        integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
+        crossorigin="anonymous"
+      />
+    </Helmet>
+    <div className="heroContainer">
+      <Img alt={data.name} fluid={data.heroImage.fluid} />
+      <div className="bio">
+        <div className="bio-content">
+          <p className="pre">Independent</p>
+          <p className="strong">Web Designer</p>
+          <p className="strong">Full Stack Developer</p>
+          <p className="strong">Digital Strategist</p>
+          <p className="cta">
+            I build bad-ass websites that are hella performant. Hire me and
+            let’s build something you can brag about.
+          </p>
+          <button>Talk to me</button>
+        </div>
         {/* 
         <h3>{data.name}</h3>
         <p>{data.title}</p>
         <p>{data.shortBio.shortBio}</p> */}
       </div>
+    </div>
+    <div className="bottomLinks">
+      <a href="https://github.com/Putnam14">
+        <i className="fab fa-github" />
+      </a>
+      <a href="https://twitter.com/BridgerPutnam">
+        <i className="fab fa-twitter" />
+      </a>
+      <Link to="/#content" className="downArrow">
+        <i className="downArrow fas fa-angle-double-down" />
+      </Link>
     </div>
   </HeroStyles>
 )
--- src/components/styles/HeroStyles.js
+++ src/components/styles/HeroStyles.js
@@ -1,16 +1,65 @@
 import styled from 'styled-components'
 
 const HeroStyles = styled.div`
-  display: grid;
   height: 100vh;
   width: 100vw;
-  grid-template-columns: 1fr 1fr;
-  .bio {
-    display: grid;
-    align: center;
-    justify-items: center;
-    align-items: center;
+  .heroContainer {
     height: 100%;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    background-image: linear-gradient(white 90%, ${props => props.theme.white});
+    .bio {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    .bio-content {
+      max-width: 50%;
+    }
+    p {
+      margin: 0;
+      padding: 0;
+      font-size: 1.5em;
+    }
+    .pre {
+      color: ${props => props.theme.lightGray};
+      margin-bottom: -0.5em;
+    }
+    .strong {
+      font-weight: 900;
+      margin-bottom: -0.25em;
+    }
+    .cta {
+      margin: 1em 0 1em;
+    }
+    button {
+      border: 0;
+      background-color: ${props => props.theme.base};
+      color: ${props => props.theme.white};
+      font-size: 1rem;
+      border-radius: 4px;
+      box-shadow: ${props => props.theme.bs};
+      height: 3rem;
+      width: 50%;
+    }
+  }
+  .bottomLinks {
+    position: absolute;
+    bottom: 0;
+    display: flex;
+    width: 100%;
+    i {
+      color: ${props => props.theme.lightGray};
+      opacity: 0.8;
+      padding: 0 0 1rem 3rem;
+      font-size: 3em;
+    }
+    .downArrow {
+      margin-left: auto;
+      padding-right: 3rem;
+      opacity: 1;
+      color: ${props => props.theme.base};
+    }
   }
 `
 
