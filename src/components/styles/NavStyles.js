8e6272e09492f80a010fcb1fecd09994950b113d
Putnam14 <putnambr76@gmail.com>
2018-12-27 19:21:59 -0700

    Style the nav as a ul

--- src/components/styles/NavStyles.js
+++ src/components/styles/NavStyles.js
@@ -1,31 +1,37 @@
 import styled from 'styled-components'
 
-const NavStyles = styled.div`
+const NavStyles = styled.ul`
+  z-index: 1;
   position: fixed;
   top: 0;
-  z-index: 1;
+  left: 0;
+  display: flex;
   width: 100vw;
-  display: grid;
-  grid-template-columns: auto 1fr;
-  justify-items: end;
-  ul {
-    display: flex;
-    justify-content: center;
-    list-style: none;
-    padding: 0;
-    margin: 0;
-    height: 20vh;
-    max-height: 100px;
-    font-size: 1.25em;
-  }
+  background-color: rgba(0, 0, 0, 0.2);
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  font-size: 1.25em;
+  height: 100px;
+  align-items: center;
   li {
-    display: inline-flex;
-    align-items: center;
-    margin: 0 1em;
-    a {
-      text-decoration: none;
-      color: currentColor;
-    }
+    margin: 0 1em 0 1em;
+  }
+  .name {
+    font-size: 2em;
+    font-weight: bold;
+    color: white;
+    margin-right: auto;
+    margin-left: 3em;
+  }
+  .last {
+    padding-right: 3em;
+  }
+  .active {
+    border-bottom: 5px solid orange;
+  }
+  a {
+    text-decoration: none;
   }
 `
 
