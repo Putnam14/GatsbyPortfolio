e3859c063b42f98570255ef5316909e4b76ee00a
Putnam14 <putnambr76@gmail.com>
2018-12-28 23:59:54 -0700

    Specify quality of 100 for hero image

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,7 +36,7 @@ export const pageQuery = graphql`
       brandingTitle
       siteTitle
       heroImage {
-        fluid(background: "rgb:000000") {
+        fluid(quality: 100) {
           ...GatsbyContentfulFluid_tracedSVG
         }
       }
