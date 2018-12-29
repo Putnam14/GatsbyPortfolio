0b703273fc379c2af0e3bf7f9d488181ca193cdf
Putnam14 <putnambr76@gmail.com>
2018-12-29 00:40:56 -0700

    Check for a hero image, move author info to the bottom

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -13,26 +13,29 @@ class BlogPostTemplate extends React.Component {
     const siteTitle = get(this.props, 'data.site.siteMetadata.title')
 
     return (
-      <Layout location={this.props.location} >
+      <Layout location={this.props.location}>
         <div style={{ background: '#fff' }}>
           <Helmet title={`${post.title} | ${siteTitle}`} />
-          <div className={heroStyles.hero}>
-            <Img className={heroStyles.heroImage} alt={post.title} fluid={post.heroImage.fluid} />
-          </div>
+          {post.heroImage.fluid && (
+            <div className={heroStyles.hero}>
+              <Img
+                className={heroStyles.heroImage}
+                alt={post.title}
+                fluid={post.heroImage.fluid}
+              />
+            </div>
+          )}
           <div className="wrapper">
             <h1 className="section-headline">{post.title}</h1>
-            <p
-              style={{
-                display: 'block',
-              }}
-            >
-              {post.publishDate}
-            </p>
             <div
               dangerouslySetInnerHTML={{
                 __html: post.body.childMarkdownRemark.html,
               }}
             />
+          </div>
+          <div className="section-headline" />
+          <div className="author-info">
+            <p>{post.publishDate}</p>
           </div>
         </div>
       </Layout>
