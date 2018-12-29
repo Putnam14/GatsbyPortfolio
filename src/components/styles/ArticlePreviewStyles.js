bb4e743a0e26f5ba5abad119f73c77d83853b91d
Putnam14 <putnambr76@gmail.com>
2018-12-28 20:13:28 -0700

    Missing semicolon...

--- src/components/styles/ArticlePreviewStyles.js
+++ src/components/styles/ArticlePreviewStyles.js
@@ -6,7 +6,7 @@ const ArticlePreviewStyles = styled.li`
     z-index: 0;
   }
   .card {
-    position: relative
+    position: relative;
     background: ${props => props.theme.cardBg};
     margin-top: -4em;
     z-index: 1;
@@ -15,18 +15,18 @@ const ArticlePreviewStyles = styled.li`
     padding-top: 1em;
   }
   .tag {
-      display: block;
-      color: ${props => props.theme.lightGray};
-      padding: 0.25em;
-      margin: 0;
-      border-left: 4px solid ${props => props.theme.accent};
-      text-transform: capitalize;
+    display: block;
+    color: ${props => props.theme.lightGray};
+    padding: 0.25em;
+    margin: 0;
+    border-left: 4px solid ${props => props.theme.accent};
+    text-transform: capitalize;
   }
   h3 {
-      margin: 0;
-      padding-left: 0.5em;
-      color: ${props => props.theme.base};
-      font-size: 1.5em;
+    margin: 0;
+    padding-left: 0.5em;
+    color: ${props => props.theme.base};
+    font-size: 1.5em;
   }
   a {
     text-decoration: none;
@@ -36,23 +36,23 @@ const ArticlePreviewStyles = styled.li`
     margin: 0;
   }
   small {
-      display: block;
-      padding: 1em;
+    display: block;
+    padding: 1em;
     color: ${props => props.theme.lightGray};
   }
-  :nth-child(even){
-      .card {
-        border-radius: 16px 0 16px 16px;
-        margin-left: auto;
-      }
-      .tag {
-        margin-left: auto;
-        text-align: right;
-        border-left: 0;
-        border-right: 4px solid ${props => props.theme.accent};
+  :nth-child(even) {
+    .card {
+      border-radius: 16px 0 16px 16px;
+      margin-left: auto;
+    }
+    .tag {
+      margin-left: auto;
+      text-align: right;
+      border-left: 0;
+      border-right: 4px solid ${props => props.theme.accent};
     }
     small {
-        text-align: right;
+      text-align: right;
     }
   }
 `
