3db6af31e73495a12aaaa913e27837f306a6105c
Putnam14 <putnambr76@gmail.com>
2018-12-29 00:06:40 -0700

    Mobile friendliness

--- src/components/BlogHeader.js
+++ src/components/BlogHeader.js
@@ -5,12 +5,23 @@ const Header = styled.div`
   padding: 6rem 6rem 1rem 6rem;
   background-color: white;
   display: flex;
-  div {
-    margin-left: auto;
+  @media (max-width: 739px) {
+    width: 100%;
+    margin: 0;
+    padding: 1rem 0 0 0;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+  @media (min-width: 740px) {
+    div {
+      margin-left: auto;
+    }
+    span {
+      padding-left: 2rem;
+    }
   }
   span {
     font-weight: bold;
-    padding-left: 2rem;
     color: ${props => props.theme.base};
   }
 `
