770ed7a36d365deb69df1560d62ac463aff406c5
Putnam14 <putnambr76@gmail.com>
2018-12-28 21:44:39 -0700

    First steps towards mobile friendliness

--- src/components/styles/NavStyles.js
+++ src/components/styles/NavStyles.js
@@ -5,36 +5,46 @@ const NavStyles = styled.ul`
   position: fixed;
   top: 0;
   left: 0;
-  display: flex;
   width: 100vw;
   margin: 0;
   padding: 0;
   list-style: none;
-  font-size: 1.25em;
-  font-weight: 600;
-  height: 4rem;
-  align-items: center;
   background-color: rgba(161, 162, 167, 0.8);
   box-shadow: ${props => props.theme.bs};
   -webkit-backdrop-filter: blur(2px);
-  li {
-    margin: 0 1em 0 1em;
-  }
   .name {
     font-weight: 900;
     font-size: 2em;
     color: ${props => props.theme.base};
-    margin-right: auto;
-    margin-left: 3em;
-  }
-  .last {
-    padding-right: 3em;
-  }
-  .active {
-    border-bottom: 5px solid ${props => props.theme.accent};
   }
   a {
     text-decoration: none;
+  }
+  @media (max-width: 739px) {
+    display: grid;
+    justify-items: center;
+    .active {
+      border-bottom: 2px solid ${props => props.theme.accent};
+    }
+  }
+  @media (min-width: 740px) {
+    display: flex;
+    font-size: 1.25em;
+    font-weight: 600;
+    height: 4rem;
+    align-items: center;
+    li {
+      margin: 0 0 0 1em;
+    }
+    .name {
+      margin-right: auto;
+    }
+    .last {
+      padding-right: 3em;
+    }
+    .active {
+      border-bottom: 5px solid ${props => props.theme.accent};
+    }
   }
 `
 
