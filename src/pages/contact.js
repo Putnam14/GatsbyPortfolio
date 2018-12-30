5ed6342859c401a74020b2547c85226e674c4d91
Putnam14 <putnambr76@gmail.com>
2018-12-30 15:44:25 -0700

    Remmove unused imports

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import { Link, graphql } from 'gatsby'
 import Helmet from 'react-helmet'
-import styles from './blog.module.css'
 import Layout from '../components/layout'
 import Contact from '../components/Contact'
 
