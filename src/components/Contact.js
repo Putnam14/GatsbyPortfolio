1da7df15e6863dabb67b9e8ef44fd443a817815d
Putnam14 <putnambr76@gmail.com>
2019-03-31 02:09:38 -0600

    Make Contact stateful, post to my email-sending API

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,36 +2,77 @@ import React from 'react'
 import HalfRule from './styles/HalfRule'
 import ContactStyles from './styles/ContactStyles'
 
-const Contact = () => (
-  <div className="wrapper">
-    <HalfRule>
-      <span>Contact Me</span>
-    </HalfRule>
-    <ContactStyles>
-      <p>
-        Don't be afraid to contact me on{' '}
-        <a href="https://twitter.com/BridgerPutnam">Twitter</a> or{' '}
-        <a href="mailto:hey+portfolio@bridgerputnam.me">email</a>.
-      </p>
-      <p>Alternatively, you can use the form below.</p>
-      <form name="contact" netlify>
-        <label>
-          Name <input type="text" name="name" placeholder="Your name..." />
-        </label>
-        <label>
-          Email <input type="email" name="email" placeholder="Your email..." />
-        </label>
-        <label>
-          Message{' '}
-          <textarea
-            name="message"
-            placeholder="Hi Bridger, I saw your portfolio and..."
-          />
-        </label>
-        <button type="submit">Send</button>
-      </form>
-    </ContactStyles>
-  </div>
-)
+class Contact extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { email: '', name: '', subject: '', body: '' }
+  }
+  handleChange = event => {
+    const target = event.target
+    const name = target.name
+    this.setState({ [name]: target.value })
+  }
+  handleSubmit = e => {
+    e.preventDefault()
+    console.log(e.target)
+    fetch('http://mail.bridgerputnam.me', {
+      method: 'post',
+      headers: new Headers({
+        Authorization: 'Basic ' + btoa('bridgerputnam:test'),
+        'Content-Type': 'application/x-www-form-urlencoded',
+      }),
+      body: this.state,
+    })
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <HalfRule>
+          <span>Contact Me</span>
+        </HalfRule>
+        <ContactStyles>
+          <p>
+            Don't be afraid to contact me on{' '}
+            <a href="https://twitter.com/BridgerPutnam">Twitter</a> or{' '}
+            <a href="mailto:hey+portfolio@bridgerputnam.me">email</a>.
+          </p>
+          <p>Alternatively, you can use the form below.</p>
+          <form name="contact" onSubmit={this.handleSubmit}>
+            <label>
+              Name{' '}
+              <input
+                type="text"
+                name="name"
+                placeholder="Your name..."
+                value={this.state.name}
+                onChange={this.handleChange}
+              />
+            </label>
+            <label>
+              Email{' '}
+              <input
+                type="email"
+                name="email"
+                placeholder="Your email..."
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+            </label>
+            <label>
+              Message{' '}
+              <textarea
+                name="body"
+                placeholder="Hi Bridger, I saw your portfolio and..."
+                value={this.state.body}
+                onChange={this.handleChange}
+              />
+            </label>
+            <button type="submit">Send</button>
+          </form>
+        </ContactStyles>
+      </div>
+    )
+  }
+}
 
 export default Contact
